3f350620158751a42f4616a5638ff5517a9b0b40
abhithegreatII <abhishek013@yahoo.com>
2019-10-13 17:42:14 +0200

    German language added ; php opens in new tab

--- new/script.js
+++ new/script.js
@@ -92,3 +92,17 @@ class TextScramble {
   function setLang(lang) {
     document.getElementsByTagName('html')[0].setAttribute('lang',lang);
   }
+
+
+
+  //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
